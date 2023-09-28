1f593d3fe5efa10bb9956e69b9bb6b355e9c57ca
Eliel Moura <eliellpf@gmail.com>
2023-09-28 09:54:36 -0300

    finalizando exercicio

--- index.php
+++ index.php
@@ -3,24 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 01</title>
+    <title>Exercício 02</title>
 </head>
 <body>
     <?php
-    require_once "src/Livro.php";
-    $livro1 = new Livro();
-    $livro1->setTitulo("O pequeno Grande Principeso");
-    $livro1->setPaginas(250);
-    $livro1->setAutor("Tiuliel");
+require_once "src/Tecnico.php";
+require_once "src/Programacao.php";
+require_once "src/Didatico.php";
 
-    ?>
+$livroTecnico = new Tecnico;
+$livroTecnico->setTitulo("ADS");
+$livroTecnico->setAutor("fulano");
+$livroTecnico->setPaginas(250);
+$livroTecnico->setFormato(["Digital"]);
 
-    <p>Título: <?=$livro1->getTitulo()?></p>
-    <p>Páginas: <?=$livro1->getPaginas()?></p>
-    <p>Autor: <?=$livro1->getAutor()?></p>
+$livroTecnico2 = new Programacao;
+$livroTecnico2->setTitulo("Programação e Algoritmos");
+$livroTecnico2->setAutor("Beltrano");
+$livroTecnico2->setPaginas(450);
+$livroTecnico2->setFormato(["Fisico"]);
 
+$livroTecnico3 = new Didatico;
+$livroTecnico3->setTitulo("O pequeno grande principeso");
+$livroTecnico3->setAutor("Ciclano");
+$livroTecnico3->setPaginas(2250);
+$livroTecnico3->setFormato(["Digital"]);
 
-    
+?>
+
+<h2>Dados (Técnico)</h2>
+<p>Nome: <?=$livroTecnico->getTitulo()?></p>
+<p>Autor: <?=$livroTecnico->getAutor()?></p>
+<p>Formato: <?=$livroTecnico->getFormato()?></p>
+
+<p>Nome: <?=$livroTecnico2->getTitulo()?></p>
+<p>Autor: <?=$livroTecnico2->getAutor()?></p>
+<p>Formato: <?=$livroTecnico2->getFormato()?></p>
+
+<p>Nome: <?=$livroTecnico3->getTitulo()?></p>
+<p>Autor: <?=$livroTecnico3->getAutor()?></p>
+<p>Formato: <?=$livroTecnico3->getFormato()?></p>
+
 
 </body>
 </html>
